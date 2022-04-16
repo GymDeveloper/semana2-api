e806deac91b32018a0323bec2ba3ffc99798f309
linder3hs <linderhassinger00@gmail.com>
2022-04-16 12:46:08 -0500

    update and destroy

--- src/components/user/controller.js
+++ src/components/user/controller.js
@@ -36,3 +36,48 @@ export const store = (req, res) => {
     data: user,
   });
 };
+
+// update user
+export const update = (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone_number } = req.body;
+
+  const user = data.find((u) => u.id === Number(id));
+
+  if (!user) {
+    return res.status(200).json({
+      ok: false,
+      data: "User not found",
+    });
+  }
+
+  user.name = name;
+  user.email = email;
+  user.phone_number = phone_number;
+
+  return res.status(200).json({
+    ok: true,
+    data: user,
+  });
+};
+
+// delete user
+export const destroy = (req, res) => {
+  const { id } = req.params;
+
+  const user = data.find((u) => u.id === Number(id));
+
+  if (!user) {
+    return res.status(200).json({
+      ok: false,
+      data: "User not found",
+    });
+  }
+
+  data.splice(data.indexOf(user), 1);
+
+  return res.status(200).json({
+    ok: true,
+    data: "User deleted",
+  });
+};
