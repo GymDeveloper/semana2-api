c47515f1daf6b1b46cb613a7384e1e3235aa99b9
linder3hs <linderhassinger00@gmail.com>
2022-04-16 12:38:20 -0500

    jest

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../src/app";
 
+// GET
 describe("Lista de Usuarios", () => {
   test("METHOD GET", async () => {
     const result = await request(app).get("/user");
@@ -10,3 +11,18 @@ describe("Lista de Usuarios", () => {
   });
 });
 
+// POST
+describe("Creacion de usuario", () => {
+  test("METHOD POST", async () => {
+    const body = {
+      name: "Linder Hassinger",
+      email: "linder@gmail.com",
+      phone_number: "999999",
+    };
+
+    const result = await request(app).post("/user/store").send(body);
+
+    expect(result.status).toBe(201);
+    expect(result.ok).toBe(true);
+  });
+});
